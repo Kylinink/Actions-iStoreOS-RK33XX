cebd7c003a702563318e96ab18f046fe01cc55f4
Kylin <37763338+Kylinink@users.noreply.github.com>
2025-04-14 13:47:15 +0800

    Update u-boot.mk

--- configfiles/u-boot.mk
+++ configfiles/u-boot.mk
@@ -2,10 +2,7 @@ PKG_NAME ?= u-boot
 
 ifndef PKG_SOURCE_PROTO
 PKG_SOURCE = $(PKG_NAME)-$(PKG_VERSION).tar.bz2
-PKG_SOURCE_URL = \
-	https://github.com/xiaomeng9597/files/releases/tag/u-boot-2021.07 \
-	https://xiaomeng9597.github.io/files \
-	https://raw.githubusercontent.com/xiaomeng9597/files/main
+PKG_SOURCE_URL = https://github.com/xiaomeng9597/files/releases/download/u-boot-2021.07
 endif
 
 PKG_BUILD_DIR = $(BUILD_DIR)/$(PKG_NAME)-$(BUILD_VARIANT)/$(PKG_NAME)-$(PKG_VERSION)
